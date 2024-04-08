d3b91f6dae9a88db33d88272535169d5310cf0c1
JM <jmnavarro@gmail.com>
2024-04-08 11:29:52 -0400

    returns from hookdeck is handled by Vercel.

--- index.ts
+++ index.ts
@@ -21,76 +21,71 @@ export function withHookdeck(config: any, f: Function) {
       );
 
       if (matching.length > 0) {
-        if (typeof request.headers['x-hookdeck-eventid'] !== 'string') {
-          // first call, forward to Hookdeck
+        // first call, forward to Hookdeck
 
-          // check if there are multiple matches with the same
-          // api_key and source_name
+        // check if there are multiple matches with the same
+        // api_key and source_name
 
-          const used = new Map<string, [any]>();
+        const used = new Map<string, [any]>();
 
-          for (const result of matching) {
-            const api_key = result.api_key || process.env.HOOKDECK_API_KEY;
-            const source_name = result.source_name;
+        for (const result of matching) {
+          const api_key = result.api_key || process.env.HOOKDECK_API_KEY;
+          const source_name = result.source_name;
 
-            if (!api_key) {
-              console.error(
-                "Hookdeck API key doesn't found. You must set it as a env variable named HOOKDECK_API_KEY or include it in your hookdeck.config.js file.",
-              );
-              return f.apply(this, args);
-            }
-
-            if (!source_name) {
-              console.error(
-                "Hookdeck Source name doesn't found. You must include it in your hookdeck.config.js file.",
-              );
-              return f.apply(this, args);
-            }
-
-            const match_key = `${api_key}/${source_name}`;
-            const array = used[match_key] ?? [];
-            array.push(result);
-            used[match_key] = array;
-          }
-          const promises: Promise<any>[] = [];
-          for (const array of Object.values(used)) {
-            const used_connection_ids: string[] = [];
-            if ((array as [any]).length > 1) {
-              // If there is more than one similar match, we need the connection_id
-              // to pick out the right connection
-              for (const entry of array) {
-                if (!!entry.id && !used_connection_ids.includes(entry.id)) {
-                  const api_key = entry.api_key || process.env.HOOKDECK_API_KEY;
-                  const source_name = entry.source_name;
-                  promises.push(forwardToHookdeck(request, api_key, source_name, entry.id));
-                  used_connection_ids.push(entry.id);
-                }
-              }
-              if (promises.length === 0) {
-                console.warn(
-                  'Found indistinguishable source names, could not process',
-                  array[0].source_name,
-                );
-              }
-            } else {
-              const api_key = array[0].api_key || process.env.HOOKDECK_API_KEY;
-              const source_name = array[0].source_name;
-              promises.push(forwardToHookdeck(request, api_key, source_name));
-            }
-          }
-          if (promises.length === 0) {
-            // we can't continue here, as there is no valid connection to use
-            return new Response(
-              'withHookdeck: match_path matched, but no valid connection was found',
-              { status: 404 },
+          if (!api_key) {
+            console.error(
+              "Hookdeck API key doesn't found. You must set it as a env variable named HOOKDECK_API_KEY or include it in your hookdeck.config.js file.",
             );
+            return f.apply(this, args);
           }
-          // If several promises were fullfilled, return the first one as required by the middleware definition
-          return Promise.all(promises).then((val) => val[0]);
-        } else {
-          console.log("Hookdeck's return... calling user middleware");
-          // second call, bypass Hookdeck
+
+          if (!source_name) {
+            console.error(
+              "Hookdeck Source name doesn't found. You must include it in your hookdeck.config.js file.",
+            );
+            return f.apply(this, args);
+          }
+
+          const match_key = `${api_key}/${source_name}`;
+          const array = used[match_key] ?? [];
+          array.push(result);
+          used[match_key] = array;
         }
+        const promises: Promise<any>[] = [];
+        for (const array of Object.values(used)) {
+          const used_connection_ids: string[] = [];
+          if ((array as [any]).length > 1) {
+            // If there is more than one similar match, we need the connection_id
+            // to pick out the right connection
+            for (const entry of array) {
+              if (!!entry.id && !used_connection_ids.includes(entry.id)) {
+                const api_key = entry.api_key || process.env.HOOKDECK_API_KEY;
+                const source_name = entry.source_name;
+                promises.push(forwardToHookdeck(request, api_key, source_name, entry.id));
+                used_connection_ids.push(entry.id);
+              }
+            }
+            if (promises.length === 0) {
+              console.warn(
+                'Found indistinguishable source names, could not process',
+                array[0].source_name,
+              );
+            }
+          } else {
+            const api_key = array[0].api_key || process.env.HOOKDECK_API_KEY;
+            const source_name = array[0].source_name;
+            promises.push(forwardToHookdeck(request, api_key, source_name));
+          }
+        }
+        if (promises.length === 0) {
+          // we can't continue here, as there is no valid connection to use
+          return new Response(
+            'withHookdeck: match_path matched, but no valid connection was found',
+            { status: 404 },
+          );
+        }
+        // If several promises were fullfilled, return the first one as required by the middleware definition
+        return Promise.all(promises).then((val) => val[0]);
       } else {
         console.log('No match... calling user middleware');
         // no match, regular call
