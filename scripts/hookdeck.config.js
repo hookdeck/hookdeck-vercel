00c2fdbb8727c9df549ce545f26622e16f3e0294
JM <jmnavarro@gmail.com>
2024-04-23 11:45:41 -0400

    clean sample config

--- scripts/hookdeck.config.js
+++ scripts/hookdeck.config.js
@@ -6,13 +6,14 @@ module.exports = {
 
     // all fields below this line are optional:
     host: '',
+    url: '',
+
     retry: {
       strategy: 'exponential',
       count: 0,
       interval: 0,
     },
     delay: 0,
-    alert: '',
     filters: [
       {
         headers: {},
@@ -21,16 +22,13 @@ module.exports = {
         path: {},
       },
     ],
-    // source configuration
-    custom_response: {},
-    verification: {},
-    // destination configuration
-    url: '',
     delivery_rate: {
       limit: 100,
       period: 'minute',
     },
-    http_method: '',
-    auth_method: {},
+
+    verification: {},
+
+    custom_response: {},
   },
 };
