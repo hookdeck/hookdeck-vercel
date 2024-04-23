4b80346272f18b1833c646f25d451cef6baaa259
JM <jmnavarro@gmail.com>
2024-04-23 06:22:46 -0400

    clean and document config

--- hookdeck.config.d.ts
+++ hookdeck.config.d.ts
@@ -7,19 +7,27 @@ interface DeliveryRate {
 
 export interface SourceConfig {
   matcher: string;
+
   api_key?: string; // not recommended, use HOOKDECK_API_KEY instead
   signing_secret?: string; // not recommended, use HOOKDECK_SIGNING_SECRET instead
+
   source_name?: string;
+
+  // for advanced routing scenarios
   host?: string;
+  url?: string;
+
+  // Hookdeck basic functionallity
   retry?: Omit<Hookdeck.RetryRule, 'type'>;
   delay?: number;
   filter?: Omit<Hookdeck.FilterRule, 'type'>;
-  custom_response?: Hookdeck.SourceCustomResponse;
-  verification?: Hookdeck.SourceVerification;
-  url?: string;
   delivery_rate?: DeliveryRate;
-  http_method?: Hookdeck.SourceAllowedHttpMethod;
-  auth_method?: Hookdeck.DestinationAuthMethodConfig;
+
+  // source verification
+  verification?: Hookdeck.SourceVerification;
+
+  // tweak response
+  custom_response?: Hookdeck.SourceCustomResponse;
 }
 
 interface HookdeckConfig {
