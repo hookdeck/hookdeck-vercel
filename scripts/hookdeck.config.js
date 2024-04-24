11cefac506b87b047bc14fd6869d242fe84c327e
JM <jmnavarro@gmail.com>
2024-04-24 09:28:20 -0400

    use new config schema and add sample file

--- scripts/hookdeck.config.js
+++ scripts/hookdeck.config.js
@@ -1,34 +1,17 @@
-// TODO: See documentation here https://hookdeck.com/xxxxx
 
-module.exports = {
-    
-  vercel_url: '', // Optional
-
-  match: {
-    '/path/to/match': {  
-      // all fields below this line are optional
-      retry: {
-        strategy: 'exponential',
-        count: 0,
-        interval: 0,
-      },
+const hookdeckConfig = {
+  vercel_url: '', // optional Uses `VERCEL_BRANCH_URL` env var as default.
+  match : {
+    '/path/to/match': {
+      // all fields are optional
+      retry: {},
       delay: 0,
-      filters: [
-        {
-          headers: {},
-          body: {},
-          query: {},
-          path: {},
-        },
-      ],
-      delivery_rate: {
-        limit: 100,
-        period: 'minute',
-      },
-  
+      filters: [],
+      delivery_rate: {},
       verification: {},
-  
       custom_response: {},
     },
   }
 };
+
+module.exports = hookdeckConfig;
---
+++ scripts/hookdeck.config.sample.js
@@ -0,0 +1,41 @@
+const {
+  RetryStrategy,
+  DestinationRateLimitPeriod,
+  SourceCustomResponseContentType,
+} = require('@hookdeck/sdk/api');
+
+const hookdeckConfig = {
+  vercel_url: '',
+  match: {
+    '/path/to/match': {
+      // all fields below this line are optional
+      retry: {
+        strategy: RetryStrategy.Linear,
+        count: 0,
+        interval: 0,
+      },
+      delay: 0,
+      filters: [
+        {
+          headers: {},
+          body: {},
+          query: {},
+          path: {},
+        },
+      ],
+      delivery_rate: {
+        limit: 100,
+        period: DestinationRateLimitPeriod.Minute,
+      },
+
+      verification: {},
+
+      custom_response: {
+        contentType: SourceCustomResponseContentType.Json,
+        body: '',
+      },
+    },
+  },
+};
+
+module.exports = hookdeckConfig;
