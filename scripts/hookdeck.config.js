a5cb0fe01f3cee693706b9c9058915388cf2d6aa
Phil Leggetter <phil@leggetter.co.uk>
2024-04-26 15:07:27 +0100

    chore: add full config example + use/import types

--- scripts/hookdeck.config.js
+++ scripts/hookdeck.config.js
@@ -1,14 +1,47 @@
+// Hookdeck SDK is a dependency of Hookdeck Vercel Middleware.
+// const {
+//   RetryStrategy,
+//   DestinationRateLimitPeriod,
+//   SourceCustomResponseContentType,
+// } = require('@hookdeck/sdk/api');
+
+/** @type {import("@hookdeck/vercel").HookdeckConfig} */
 const hookdeckConfig = {
-  vercel_url: '', // optional Uses `VERCEL_BRANCH_URL` env var as default.
+  // vercel_url: '', // optional. Uses `VERCEL_BRANCH_URL` env var as default.
   match: {
-    '/path/to/match': {
-      // all fields are optional
-      retry: {},
-      delay: 0,
-      filters: [],
-      rate: {},
-      verification: {},
-      custom_response: {},
+    '/api/webhooks': {
+      // all these fields are optional
+      // retry: {
+      //   strategy: RetryStrategy.Linear,
+      //   count: 5,
+      //   interval: 1 * 60 * 1000, // in milliseconds
+      // },
+      // delay: 1 * 60 * 1000, // in milliseconds
+      // filters: [
+      //   {
+      //     headers: {
+      //       'x-my-header': 'my-value',
+      //     },
+      //     body: {},
+      //     query: {},
+      //     path: {},
+      //   },
+      // ],
+      // rate: {
+      //   limit: 10,
+      //   period: DestinationRateLimitPeriod.Minute,
+      // },
+      // verification: {
+      //   type: 'API_KEY',
+      //   configs: {
+      //     header_key: 'x-my-api-key',
+      //     api_key: 'this-is-my-token',
+      //   },
+      // },
+      // custom_response: {
+      //   content_type: SourceCustomResponseContentType.Json,
+      //   body: '{"message": "Vercel handled the webhook using Hookdeck"}',
+      // },
     },
   },
 };
